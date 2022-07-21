3e76eab21e2e02f0a71b83959089971b13251b4c
YeonDoDo <yeondoodo@gmail.com>
2022-07-21 23:21:40 +0900

    로키 나는 여기까진가봐... 힝구...뿌엥...ㅠㅠ

--- src/main/java/com/dotkkun/init/ModBlocks.java
+++ src/main/java/com/dotkkun/init/ModBlocks.java
@@ -17,5 +17,8 @@ public class ModBlocks {
     public static final Block VEGE_REFRESHER_3 = new VegeRefresher3("vege_refresher_3", Material.IRON);
     public static final Block VEGE_REFRESHER_4 = new VegeRefresher4("vege_refresher_4", Material.IRON);
     public static final Block WINDOW_MUNTIN = new WindowMuntin("muntin", Material.IRON);
+    public static final Block LECTURE_DESK_1 = new LectureDesk1("lecture_desk_1", Material.IRON);
+    public static final Block LECTURE_DESK_2 = new LectureDesk2("lecture_desk_2", Material.IRON);
+    public static final Block LECTURE_DESK_3 = new LectureDesk3("lecture_desk_3", Material.IRON);
 
 }
