1f5d168529db0690bb1b8f92bdff53745cc0367c
CHO-ROCKY <chorockyyy@gmail.com>
2022-07-26 23:15:10 +0900

    학생 책상 완성했지롱~~

--- src/main/java/com/dotkkun/init/ModBlocks.java
+++ src/main/java/com/dotkkun/init/ModBlocks.java
@@ -17,5 +17,7 @@ public class ModBlocks {
     public static final Block VEGE_REFRESHER_3 = new VegeRefresher3("vege_refresher_3", Material.IRON);
     public static final Block VEGE_REFRESHER_4 = new VegeRefresher4("vege_refresher_4", Material.IRON);
     public static final Block WINDOW_MUNTIN = new WindowMuntin("muntin", Material.IRON);
-
+    public static final Block STUDENT_DESK_1 = new StudentDesk1("student_desk_1", Material.IRON);
+    public static final Block STUDENT_DESK_2 = new StudentDesk2("student_desk_2", Material.IRON);
+    public static final Block STUDENT_DESK_3 = new StudentDesk3("student_desk_3", Material.IRON);
 }
