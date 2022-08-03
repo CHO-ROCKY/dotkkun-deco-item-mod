2a9d48e1f492562022f4c56a044a6e4063c7b8a1
CHO-ROCKY <chorockyyy@gmail.com>
2022-08-03 16:28:27 +0900

    왜 커밋이 안 됐죠 ?

--- src/main/java/com/dotkkun/blocks/TrafficRightGreen1.java
+++ src/main/java/com/dotkkun/blocks/TrafficRightGreen1.java
@@ -1,2 +1,120 @@
-package com.dotkkun.blocks;public class TrafficRightGreen1 {
+package com.dotkkun.blocks;
+
+import com.dotkkun.init.ModBlocks;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class TrafficRightGreen1 extends BlockBase{
+    public static final PropertyDirection FACING = PropertyDirection.create("facing", EnumFacing.Plane.HORIZONTAL);
+
+    public TrafficRightGreen1(String name, Material material) {
+        super(name, material);
+        setHardness(2.0f);
+        setHarvestLevel("axe", -1);
+        setResistance(18.0f);
+        setSoundType(SoundType.STONE);
+        this.setDefaultState(this.getBlockState().getBaseState().withProperty(FACING, EnumFacing.SOUTH));
+    }
+
+    @Override
+    public IBlockState getStateFromMeta(int meta)
+    {
+        IBlockState iblockstate = this.getDefaultState();
+
+        switch (meta)
+        {
+            case 1:
+                iblockstate = iblockstate.withProperty(FACING, EnumFacing.EAST);
+                break;
+            case 2:
+                iblockstate = iblockstate.withProperty(FACING, EnumFacing.WEST);
+                break;
+            case 3:
+                iblockstate = iblockstate.withProperty(FACING, EnumFacing.SOUTH);
+                break;
+            case 4:
+                iblockstate = iblockstate.withProperty(FACING, EnumFacing.NORTH);
+                break;
+            default:
+                iblockstate = iblockstate.withProperty(FACING, EnumFacing.NORTH);
+        }
+
+        return iblockstate;
+    }
+
+    @Override
+    public int getMetaFromState(IBlockState state)
+    {
+        int i = 0;
+
+        switch ((EnumFacing)state.getValue(FACING))
+        {
+            case EAST:
+                i = i | 1;
+                break;
+            case WEST:
+                i = i | 2;
+                break;
+            case SOUTH:
+                i = i | 3;
+                break;
+            case NORTH:
+                i = i | 4;
+                break;
+            default:
+                i = i | 4;
+        }
+
+        return i;
+    }
+
+    @Override
+    protected BlockStateContainer createBlockState() {
+        return new BlockStateContainer(this, new IProperty[]{FACING});
+    }
+
+    @Override
+    public boolean isFullCube(IBlockState state){
+        return false;
+    }
+
+    @Override
+    public boolean isOpaqueCube(IBlockState state){
+        return false;
+    }
+
+    @Override
+    public IBlockState getStateForPlacement(World world, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer, EnumHand hand) {
+        return this.getDefaultState().withProperty(FACING, placer.getAdjustedHorizontalFacing().getOpposite());
+    }
+
+    @Override
+    public void onBlockPlacedBy(World worldIn, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack){
+        BlockPos pos1 = pos.up();
+        BlockPos pos2 = pos1.up();
+        if(placer instanceof EntityPlayer){
+            worldIn.setBlockState(pos1, ModBlocks.TRAFFIC_RIGHT_GREEN_2.getDefaultState().withProperty(FACING, placer.getAdjustedHorizontalFacing().getOpposite()));
+            worldIn.setBlockState(pos2, ModBlocks.TRAFFIC_RIGHT_GREEN_3.getDefaultState().withProperty(FACING, placer.getAdjustedHorizontalFacing().getOpposite()));
+        }
+    }
+
+    @Override
+    public boolean removedByPlayer(IBlockState state, World world, BlockPos pos, EntityPlayer player, boolean willHarvest){
+        BlockPos pos1 = pos.up();
+        BlockPos pos2 = pos1.up();
+        world.setBlockState(pos1, net.minecraft.init.Blocks.AIR.getDefaultState(), world.isRemote ? 11 : 3);
+        world.setBlockState(pos2, net.minecraft.init.Blocks.AIR.getDefaultState(), world.isRemote ? 11 : 3);
+        return world.setBlockState(pos, net.minecraft.init.Blocks.AIR.getDefaultState(), world.isRemote ? 11 : 3);
+    }
 }
--- src/main/java/com/dotkkun/init/ModBlocks.java
+++ src/main/java/com/dotkkun/init/ModBlocks.java
@@ -17,5 +17,13 @@ public class ModBlocks {
     public static final Block VEGE_REFRESHER_3 = new VegeRefresher3("vege_refresher_3", Material.IRON);
     public static final Block VEGE_REFRESHER_4 = new VegeRefresher4("vege_refresher_4", Material.IRON);
     public static final Block WINDOW_MUNTIN = new WindowMuntin("muntin", Material.IRON);
-
+    public static final Block TRAFFIC_RIGHT_OFF_1 = new TrafficRightOff1("traffic_right_off_1", Material.IRON);
+    public static final Block TRAFFIC_RIGHT_OFF_2 = new TrafficRightOff2("traffic_right_off_2", Material.IRON);
+    public static final Block TRAFFIC_RIGHT_OFF_3 = new TrafficRightOff3("traffic_right_off_3", Material.IRON);
+    public static final Block TRAFFIC_RIGHT_GREEN_1 = new TrafficRightGreen1("traffic_right_green_1", Material.IRON);
+    public static final Block TRAFFIC_RIGHT_GREEN_2 = new TrafficRightGreen2("traffic_right_green_2", Material.IRON);
+    public static final Block TRAFFIC_RIGHT_GREEN_3 = new TrafficRightGreen3("traffic_right_green_3", Material.IRON);
+    public static final Block TRAFFIC_RIGHT_RED_1 = new TrafficRightRed1("traffic_right_red_1", Material.IRON);
+    public static final Block TRAFFIC_RIGHT_RED_2 = new TrafficRightRed2("traffic_right_red_2", Material.IRON);
+    public static final Block TRAFFIC_RIGHT_RED_3 = new TrafficRightRed3("traffic_right_red_3", Material.IRON);
 }
